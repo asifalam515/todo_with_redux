f9e8903e12cbb4ecb1f0b84b2184c04cec9adc5c
asifalam515 <asibulalam515@gmail.com>
2025-01-14 17:06:30 +0600

    add to do modal

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,20 +1,25 @@
+import { Button } from "../ui/button";
+import AddTodoModal from "./AddTodoModal";
 import TodoCard from "./TodoCard";
 
 const TodoContainer = () => {
   return (
     <div>
-      <div>
-        <button>Add Todo</button>
+      <div className="flex justify-between mb-5">
+        <AddTodoModal></AddTodoModal>
+
         <button>Filter</button>
       </div>
-      <div className="bg-red-500 w-full h-full rounded-xl p-5 space-y-5">
+      <div className="bg-red-500 w-full h-full rounded-xl space-y-5 p-[3px]">
         {/* <div className="bg-white p-3 flex justify-center items-center rounded-md text-2xl font-bold">
           <p>There is no task pending</p>
         </div> */}
-        <TodoCard></TodoCard>
-        <TodoCard></TodoCard>
-        <TodoCard></TodoCard>
-        <TodoCard></TodoCard>
+        <div className="bg-white  p-5 w-full h-full rounded-md">
+          <TodoCard></TodoCard>
+          <TodoCard></TodoCard>
+          <TodoCard></TodoCard>
+          <TodoCard></TodoCard>
+        </div>
       </div>
     </div>
   );
