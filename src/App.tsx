6d2c3acab9dfcfa6558e6c2719de474ff467a3d4
asifalam515 <asibulalam515@gmail.com>
2025-01-14 11:52:21 +0600

    shadcn added

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl">Started the project</h1>
+      <Button>My Button</Button>
+    </>
+  );
+}
+
+export default App;
