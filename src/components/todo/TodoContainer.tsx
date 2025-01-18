058faff76b1b264fdf69fbac2988df37fd045f24
asifalam515 <asibulalam515@gmail.com>
2025-01-18 19:41:51 +0600

    container restart

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,27 +1,45 @@
-import { useAppSelector } from "@/redux/features/hook";
-import { Button } from "../ui/button";
+import { useGetTodosQuery } from "@/redux/api/api";
 import AddTodoModal from "./AddTodoModal";
 import TodoCard from "./TodoCard";
 import TodoFilter from "./TodoFilter";
 
 const TodoContainer = () => {
-  const { todos } = useAppSelector((state) => state.todo);
+  const { data, isLoading, isError } = useGetTodosQuery(undefined);
+
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center h-full">
+        <p>Loading tasks...</p>
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div className="flex justify-center items-center h-full">
+        <p>Failed to load tasks. Please try again later.</p>
+      </div>
+    );
+  }
+
+  // Access the data array from the response
+  const todos = data?.data ?? [];
 
   return (
     <div>
       <div className="flex justify-between mb-5">
-        <AddTodoModal></AddTodoModal>
-
-        <TodoFilter></TodoFilter>
+        <AddTodoModal />
+        <TodoFilter />
       </div>
       <div className="bg-red-500 w-full h-full rounded-xl space-y-5 p-[3px]">
-        {/* <div className="bg-white p-3 flex justify-center items-center rounded-md text-2xl font-bold">
-          <p>There is no task pending</p>
-        </div> */}
-        <div className="bg-white  p-5 w-full h-full rounded-md">
-          {todos.map((item) => (
-            <TodoCard {...item}></TodoCard>
-          ))}
+        <div className="bg-white p-5 w-full h-full rounded-md">
+          {todos.length > 0 ? (
+            todos.map((item) => <TodoCard key={item._id} {...item} />)
+          ) : (
+            <div className="flex justify-center items-center">
+              <p>No tasks available.</p>
+            </div>
+          )}
         </div>
       </div>
     </div>
